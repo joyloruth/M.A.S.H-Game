5a20b3e4133c7ebc9d12d1c263204971d6900015
Joy LoRuth <joyloruth@gmail.com>
2021-07-14 12:48:28 -0500

    created User Model, Repository and Service class

---
+++ MASH-Game-App/src/main/java/com/joyloruth/User.java
@@ -0,0 +1,108 @@
+package com.joyloruth;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	private Long id;
+	private String name;
+    private String email;
+    private String password;
+    
+    
+    public User() {
+    	
+    }
+    
+	public User(Long id, String name, String email, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+
+
+	/**
+	 * @return the id
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long getId() {
+		return id;
+	}
+
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+    
+    
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + "]";
+	}
+
+    
+    
+}
+	
+		
+	
+	
+	
---
+++ MASH-Game-App/src/main/java/com/joyloruth/UserRepository.java
@@ -0,0 +1,9 @@
+package com.joyloruth;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long>{
+	
+}
+
+

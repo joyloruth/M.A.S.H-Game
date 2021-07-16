80b981316410c0936b62cef3bdfe8b45d7b3edd9
Joy LoRuth <joyloruth@gmail.com>
2021-07-15 22:59:40 -0500

    added Car model and html

--- MASH-Game-App/src/main/java/com/joyloruth/Models/Car.java
+++ MASH-Game-App/src/main/java/com/joyloruth/Models/Car.java
@@ -1,5 +1,72 @@
 package com.joyloruth.Models;
 
 public class Car {
+	
+	private Long carid;
+	private String carone;
+	private String cartwo;
+	private String carthree;
+	private String carfour;
+	/**
+	 * @return the carid
+	 */
+	public Long getCarid() {
+		return carid;
+	}
+	/**
+	 * @param carid the carid to set
+	 */
+	public void setCarid(Long carid) {
+		this.carid = carid;
+	}
+	/**
+	 * @return the carone
+	 */
+	public String getCarone() {
+		return carone;
+	}
+	/**
+	 * @param carone the carone to set
+	 */
+	public void setCarone(String carone) {
+		this.carone = carone;
+	}
+	/**
+	 * @return the cartwo
+	 */
+	public String getCartwo() {
+		return cartwo;
+	}
+	/**
+	 * @param cartwo the cartwo to set
+	 */
+	public void setCartwo(String cartwo) {
+		this.cartwo = cartwo;
+	}
+	/**
+	 * @return the carthree
+	 */
+	public String getCarthree() {
+		return carthree;
+	}
+	/**
+	 * @param carthree the carthree to set
+	 */
+	public void setCarthree(String carthree) {
+		this.carthree = carthree;
+	}
+	/**
+	 * @return the carfour
+	 */
+	public String getCarfour() {
+		return carfour;
+	}
+	/**
+	 * @param carfour the carfour to set
+	 */
+	public void setCarfour(String carfour) {
+		this.carfour = carfour;
+	}
+	
 
 }
